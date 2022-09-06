fbd884f73b2944f83d84736b255baeeb52b27c2d
Daniel Correia <danielgalego.dev@gmail.com>
2022-09-06 14:44:29 -0300

    reorganizing API files

--- stock-managment-api/src/main/java/com/javainterview/sms/model/ProductCategory.java
+++
@@ -1,5 +0,0 @@
-package com.javainterview.sms.model;
-
-public enum ProductCategory {
-	PRODUCT;
-}
--- stock-managment-api/src/main/java/com/javainterview/sms/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javainterview.sms.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.javainterview.sms.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, Long>{
-
-}
